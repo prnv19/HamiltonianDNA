68f0ffd636971145ea19dd1d295f7301d881c04c
prnv <cb.en.u4aie20053@cb.students.amrita.edu>
2021-08-08 16:02:00 +0530

    ver3

--- src/DNA.java
+++ src/DNA.java
@@ -2,12 +2,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/*
+ **   Java Program to generate k-mers and graph from a sequence
+ */
+
 public class DNA {
     List<String> kmer= new ArrayList<>();
     String seq;
     int[][] adj;
     int vertices;
     StringBuilder genome = new StringBuilder();
+
+    //Constructor to create k-mers and add it to a list
 
     DNA(String seq){
         this.seq = seq;
@@ -18,12 +24,14 @@ public class DNA {
         this.vertices = kmer.size();
         adj = new int[vertices][vertices];
     }
+
+    //Function to generate the adjacency matrix by comparing the prefix and suffix of each k-mer
     public void generate_adj(){
         int i = 0;
         while(i<vertices){
             int j = 0;
             while(j<vertices){
-                if( kmer.get(i).substring(1,3).equals(kmer.get(j).substring(0,2)) && i!=j){
+                if( kmer.get(i).substring(1,3).equals(kmer.get(j).substring(0,2))){
                     adj[i][j] = 1;
                 }
                 j++;
@@ -32,6 +40,7 @@ public class DNA {
         }
     }
 
+    //Function that displays the adjacency matrix
     public void display_adj(){
         for(int i = 0;i<vertices;i++){
             for(int j = 0;j<vertices;j++){
@@ -43,6 +52,7 @@ public class DNA {
         System.out.println();
     }
 
+    //Function that finds the neighbouring nodes that is connected to node 'v'
     public void find_neighbour(int v){
 
             boolean a = true;
@@ -60,6 +70,7 @@ public class DNA {
             System.out.println();
     }
 
+    //Function to display all the nodes and the corresponding k-mer
     public void show_nodes(){
         System.out.println("Displaying Nodes..");
         for(int i=0;i<vertices;i++){
@@ -69,6 +80,7 @@ public class DNA {
         System.out.println();
     }
 
+    //Function to construct the genome from a given path
     public void construct_genome(int[] path){
 
         for(int i=0;i < path.length;i=i+2){
@@ -92,12 +104,13 @@ public class DNA {
     public static void main(String[] args){
 
         DNA obj = new DNA("ATGGCGTGCA");
+
         System.out.println(obj.seq);
         System.out.println(obj.kmer);
+
         obj.generate_adj();
         obj.display_adj();
         obj.show_nodes();
         obj.find_neighbour(2);
-
     }
 }
